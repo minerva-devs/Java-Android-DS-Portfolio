5c9c4bf53806e9e1759a2f351141f08a1cf86707
minerva-devs <180361567+minerva-devs@users.noreply.github.com>
2026-06-19 20:22:25 -0600

    Update gitignore and execution batch script

--- googled_myself/run_audit.bat
+++ googled_myself/run_audit.bat
@@ -1,5 +1,5 @@
 @echo off
-cd /d "C:\Users\minef\data-audit"
-call venv\Scripts\activate
+cd /d "C:\Users\minef\tech-portfolio\googled_myself"
+call ..\venv\Scripts\activate
 python main_advanced.py
 pause
